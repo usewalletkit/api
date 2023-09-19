4cbefc326748e8fea727aecd09898366f61d432c
Tushar Soni <tusharsoni1205@gmail.com>
2023-09-19 13:04:41 -0700

    Add example script for get wallet by owner id

---
+++ examples/node/scripts/get-wallet-by-owner-id.ts
@@ -0,0 +1,14 @@
+import { WalletKit, WalletKitClient } from "@walletkit-fern/api";
+
+const wk = new WalletKitClient({
+  environment: process.env.WALLETKIT_ENVIRONMENT,
+  projectId: process.env.WALLETKIT_PROJECT_ID as string,
+  token: process.env.WALLETKIT_TOKEN,
+});
+
+const wallet = await wk.wallets.getByOwnerId({
+  network: WalletKit.Network.Base,
+  ownerId: "user-1"
+});
+
+console.log(wallet);
